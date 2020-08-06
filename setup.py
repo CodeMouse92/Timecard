aa7857cdd381d435d53b6008d2f285d405a3697e
Jason C. McDonald <jcmcdonald@mousepawmedia.com>
2020-08-06 12:47:53 -0700

    Use setuptools properly

--- setup.py
+++ setup.py
@@ -1,120 +1,58 @@
 #!/usr/bin/env python3
+from setuptools import find_packages, setup
+import pathlib
 
-# Based on https://github.com/navdeep-G/setup.py/blob/master/setup.py
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / 'README.md').read_text(encoding='utf-8')
 
-import io
-import os
-import sys
-from shutil import rmtree
-
-from setuptools import find_packages, setup, Command
-
-# Package meta-data
-NAME = 'Timecard-App'
-VERSION = '2.0.5'
-DESCRIPTION = 'Track time beautifully.'
-AUTHOR = 'Jason C. McDonald'
-EMAIL = 'codemouse92@outlook.com'
-URL = 'https://github.com/codemouse92/timecard'
-
-REQUIRES_PYTHON = '>=3.6.0'
-
-REQUIRED = ['PySide2 >= 5.11.0']
-
-here = os.path.abspath(os.path.dirname(__file__))
-
-# Import the README and use it as the long-description.
-# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
-try:
-    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
-        long_description = '\n' + f.read()
-except FileNotFoundError:
-    long_description = DESCRIPTION
-
-# Load the package's __version__.py module as a dictionary
-about = {}
-if not VERSION:
-    project_slug = NAME.lower().replace("-", "_").replace(" ", "_")
-    with open(os.path.join(here, project_slug, '__version__.py')) as f:
-        exec(f.read(), about)
-else:
-    about['__version__'] = VERSION
-
-
-class UploadCommand(Command):
-    """Support setup.py upload."""
-
-    description = 'Build and publish the package.'
-    user_options = []
-
-    @staticmethod
-    def status(s):
-        """Prints things in bold."""
-        print('\033[1m{0}\033[0m'.format(s))
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        try:
-            self.status('Removing previous builds...')
-            rmtree(os.path.join(here, 'dist'))
-        except OSError:
-            pass
-
-        self.status('Building Source and Wheel (universal) distribution...')
-        os.system(f"{sys.executable} setup.py sdist bdist_wheel --universal")
-
-        self.status('Uploading the package to PyPI via Twine...')
-        os.system('twine upload dist/*')
-
-        self.status('Pushing git tags...')
-        os.system('git tag v{0}'.format(about['__version__']))
-        os.system('git push --tags')
-
-        sys.exit()
-
-
-# Where the magic happens:
 setup(
-    name=NAME,
-    version=about['__version__'],
-    description=DESCRIPTION,
+    name='Timecard-App',
+    version='2.0.5',
+    description='Track time beautifully.',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    author=AUTHOR,
-    author_email=EMAIL,
-    python_requires=REQUIRES_PYTHON,
-    url=URL,
-    packages=find_packages(),
-    entry_points={
-          'gui_scripts': [
-              'timecard-app = timecard.__main__:main'
-          ]
-      },
-    install_requires=REQUIRED,
-    include_package_data=True,
-    license='BSD-3-Clause',
+    author='Jason C. McDonald',
+    author_email='codemouse92@outlook.com',
+    url='https://github.com/codemouse92/timecard',
+    project_urls={
+        'Bug Reports': 'https://github.com/codemouse92/timecard/issues',
+        'Funding': 'https://github.com/sponsors/CodeMouse92',
+        'Source': 'https://github.com/codemouse92/timecard',
+    },
+    keywords='time, tracking, office, clock, tool, utility',
+
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: X11 Applications :: Qt',
-        'Intended Audience :: End Users/Desktop',
-        'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
         'Operating System :: OS Independent',
+        'Intended Audience :: End Users/Desktop',
+        'Topic :: Office/Business',
+        'License :: OSI Approved :: BSD License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: Implementation :: CPython',
-        'Topic :: Office/Business'
         ],
-    # $ setup.py publish support.
-    cmdclass={
-        'upload': UploadCommand,
-        },
-    )
+
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+
+    include_package_data=True,
+
+    python_requires='>=3.6, <4',
+    install_requires=['PySide2 >= 5.11.0'],
+
+    extras_require={
+        'test': ['pytest'],
+    },
+
+    entry_points={
+          'gui_scripts': [
+              'Timecard-App = timecard.__main__:main'
+          ]
+      }
+)
--- timecard-app-folder.spec
+++ timecard-app-folder.spec
@@ -3,7 +3,9 @@
 block_cipher = None
 
 added_files = [
-    ( 'src/timecard/resources/*', 'timecard/resources')
+    ( 'README.md', '.' ),
+    ( 'LICENSE', '.' ),
+    ( 'src/timecard/resources/*', 'timecard/resources' )
 ]
 
 a = Analysis(['src/timecard-app.py'],
@@ -22,15 +24,19 @@ pyz = PYZ(a.pure, a.zipped_data,
              cipher=block_cipher)
 exe = EXE(pyz,
           a.scripts,
-          a.binaries,
-          a.zipfiles,
-          a.datas,
           [],
+          exclude_binaries=True,
           name='timecard-app',
           debug=False,
           bootloader_ignore_signals=False,
           strip=False,
           upx=True,
-          upx_exclude=[],
-          runtime_tmpdir=None,
           console=False )
+coll = COLLECT(exe,
+               a.binaries,
+               a.zipfiles,
+               a.datas,
+               strip=False,
+               upx=True,
+               upx_exclude=[],
+               name='timecard-app')
